1bf8b2d91ef3841dd752e39368aaacd9418375fb
Nevillested <olegsidnev@gmail.com>
2023-05-04 19:23:01 +0400

    + create scheduler file

--- PyBot.py
+++ PyBot.py
@@ -6,109 +6,15 @@ import queries_to_bd
 import answer_cases
 import inline_mode
 import threading
+import scheduler
 import inspect
 import telebot
 import sending
-import datetime
 import my_cfg
-import time
 import random
 
-#Метод для шедулера - это будет дочерний поток
-def time_schedule_bot():
-    while True:
-
-        time.sleep(1)
-        cur_date_time = datetime.datetime.now()
-        
-        #if (cur_date_time.second % 2 == 0):
-        #    
-        #    #отправка рандомного сердечка
-        #    try:
-        #        cur_send_mode = 'default_mode'
-        #        cur_type_data = 'text'
-        #        heart_string = '❤️🤍💗💚💙💕💝💘💖💞🤎❣️🖤💓♥️🧡❤️'
-        #        cur_text_data = random.choice(heart_string), None, None, None
-        #        sending.send_msg(bot             = MypyBot,
-        #                         send_mode       = cur_send_mode,
-        #                         chat_id_out     = 83729683,
-        #                         type_data_out   = cur_type_data,
-        #                         text_data_out   = cur_text_data)
-
-        #        cur_text_data = random.choice(heart_string), None, None, None
-        #        sending.send_msg(bot             = MypyBot,
-        #                         send_mode       = cur_send_mode,
-        #                         chat_id_out     = my_cfg.id_owner,
-        #                         type_data_out   = cur_type_data,
-        #                         text_data_out   = cur_text_data)
-        #    except Exception as e:
-        #        print('Heart is not sending')
-                
-        #    print('Отработка ежесекундного шедулера')
-        
-        if (cur_date_time.minute == 00 and cur_date_time.second == 00):
-            
-            #отправка пикчи
-            list_users_id = queries_to_bd.get_users_id_of_current_subscription('Пикча с Шинобу')
-            for item in list_users_id:
-                try:
-                    cur_send_mode = 'default_mode'
-                    cur_type_data = 'photo'
-                    cur_photo_data = common_methods.get_pikcha(), None, 'Ежечасное солнышко\n' + "||Управление подписками:\n/managesubscriptions||" , 'MarkdownV2'
-                    sending.send_msg(bot             = MypyBot,
-                                     send_mode       = cur_send_mode,
-                                     chat_id_out     = item,
-                                     type_data_out   = cur_type_data,
-                                     photo_data_out  = cur_photo_data)
-                                         
-                except Exception as e:
-                    print('Не отправлено сообщение этому пользователю: ' +str(item)+'. Текст ошибки:\n'+str(e))
-
-            #отправка комплимента
-            list_users_id = queries_to_bd.get_users_id_of_current_subscription('Комплименты девушке')
-            for item in list_users_id:
-                try:
-                    cur_type_data = 'text'
-                    reply_markup_out = keyboards_buttons.create_inline_kb({"see_subscriptions": "Смотреть подписки"})
-                    cur_text_data = queries_to_bd.get_compliment(), reply_markup_out, None, None
-                    sending.send_msg(bot             = MypyBot,
-                                     send_mode       = cur_send_mode,
-                                     chat_id_out     = item,
-                                     type_data_out   = cur_type_data,
-                                     text_data_out   = cur_text_data)
-
-                except Exception as e:
-                    print('Не отправлено сообщение этому пользователю: ' +str(item)+'. Текст ошибки:\n'+str(e))
-
-            print('Отработка ежечасного шедулера')
-        
-        if (cur_date_time.hour == 22 and cur_date_time.minute == 00 and cur_date_time.second == 00):
-            
-            #отправка праздника
-            today_holiday = queries_to_bd.get_holiday()
-            if len(today_holiday) > 0:
-                list_users_id = queries_to_bd.get_users_id_of_current_subscription('Международные праздники')
-                today_holiday = 'Сегодня ' + today_holiday.lower() + '🎉\nС праздничком:)'
-                for item in list_users_id:
-                    try:
-                        cur_send_mode = 'default_mode'
-                        cur_type_data = 'text'
-                        reply_markup_out = keyboards_buttons.create_inline_kb({"see_subscriptions": "Смотреть подписки"})
-                        cur_text_data = today_holiday, reply_markup_out, None, None
-                        sending.send_msg(bot             = MypyBot,
-                                         send_mode       = cur_send_mode,
-                                         chat_id_out     = item,
-                                         type_data_out   = cur_type_data,
-                                         text_data_out   = cur_text_data)
-
-                    except Exception as e:
-                        print('Не отправлено сообщение этому пользователю: ' +str(item)+'. Текст ошибки:\n'+str(e))
-            print('Отработка ежедневного шедулера')
-
-
-#создаем дочерний поток на шедулер
-child_thread = threading.Thread(target=time_schedule_bot)
-#запускаем дочерний поток
+#создаем дочерний поток на шедулер и запускаем его
+child_thread = threading.Thread(target=scheduler.time_schedule_bot)
 child_thread.start()
 
 #далее ниже идет выполнение кода в основном потоке
---
+++ scheduler.py
@@ -0,0 +1,94 @@
+import time
+import datetime
+
+#Метод для шедулера - это будет дочерний поток
+def time_schedule_bot():
+    while True:
+
+        time.sleep(1)
+        cur_date_time = datetime.datetime.now()
+
+        #if (cur_date_time.second % 2 == 0):
+        #
+        #    #отправка рандомного сердечка
+        #    try:
+        #        cur_send_mode = 'default_mode'
+        #        cur_type_data = 'text'
+        #        heart_string = '❤️🤍💗💚💙💕💝💘💖💞🤎❣️🖤💓♥️🧡❤️'
+        #        cur_text_data = random.choice(heart_string), None, None, None
+        #        sending.send_msg(bot             = MypyBot,
+        #                         send_mode       = cur_send_mode,
+        #                         chat_id_out     = 83729683,
+        #                         type_data_out   = cur_type_data,
+        #                         text_data_out   = cur_text_data)
+
+        #        cur_text_data = random.choice(heart_string), None, None, None
+        #        sending.send_msg(bot             = MypyBot,
+        #                         send_mode       = cur_send_mode,
+        #                         chat_id_out     = my_cfg.id_owner,
+        #                         type_data_out   = cur_type_data,
+        #                         text_data_out   = cur_text_data)
+        #    except Exception as e:
+        #        print('Heart is not sending')
+
+        #    print('Отработка ежесекундного шедулера')
+
+        if (cur_date_time.minute == 00 and cur_date_time.second == 00):
+
+            #отправка пикчи
+            list_users_id = queries_to_bd.get_users_id_of_current_subscription('Пикча с Шинобу')
+            for item in list_users_id:
+                try:
+                    cur_send_mode = 'default_mode'
+                    cur_type_data = 'photo'
+                    cur_photo_data = common_methods.get_pikcha(), None, 'Ежечасное солнышко\n' + "||Управление подписками:\n/managesubscriptions||" , 'MarkdownV2'
+                    sending.send_msg(bot             = MypyBot,
+                                     send_mode       = cur_send_mode,
+                                     chat_id_out     = item,
+                                     type_data_out   = cur_type_data,
+                                     photo_data_out  = cur_photo_data)
+
+                except Exception as e:
+                    print('Не отправлено сообщение этому пользователю: ' +str(item)+'. Текст ошибки:\n'+str(e))
+
+            #отправка комплимента
+            list_users_id = queries_to_bd.get_users_id_of_current_subscription('Комплименты девушке')
+            for item in list_users_id:
+                try:
+                    cur_type_data = 'text'
+                    reply_markup_out = keyboards_buttons.create_inline_kb({"see_subscriptions": "Смотреть подписки"})
+                    cur_text_data = queries_to_bd.get_compliment(), reply_markup_out, None, None
+                    sending.send_msg(bot             = MypyBot,
+                                     send_mode       = cur_send_mode,
+                                     chat_id_out     = item,
+                                     type_data_out   = cur_type_data,
+                                     text_data_out   = cur_text_data)
+
+                except Exception as e:
+                    print('Не отправлено сообщение этому пользователю: ' +str(item)+'. Текст ошибки:\n'+str(e))
+
+            print('Отработка ежечасного шедулера')
+
+        if (cur_date_time.hour == 22 and cur_date_time.minute == 00 and cur_date_time.second == 00):
+
+            #отправка праздника
+            today_holiday = queries_to_bd.get_holiday()
+            if len(today_holiday) > 0:
+                list_users_id = queries_to_bd.get_users_id_of_current_subscription('Международные праздники')
+                today_holiday = 'Сегодня ' + today_holiday.lower() + '🎉\nС праздничком:)'
+                for item in list_users_id:
+                    try:
+                        cur_send_mode = 'default_mode'
+                        cur_type_data = 'text'
+                        reply_markup_out = keyboards_buttons.create_inline_kb({"see_subscriptions": "Смотреть подписки"})
+                        cur_text_data = today_holiday, reply_markup_out, None, None
+                        sending.send_msg(bot             = MypyBot,
+                                         send_mode       = cur_send_mode,
+                                         chat_id_out     = item,
+                                         type_data_out   = cur_type_data,
+                                         text_data_out   = cur_text_data)
+
+                    except Exception as e:
+                        print('Не отправлено сообщение этому пользователю: ' +str(item)+'. Текст ошибки:\n'+str(e))
+            print('Отработка ежедневного шедулера')
+
